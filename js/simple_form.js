0d8e35811f657145dcfd9d3ef33962eaed15ceb8
mandira-05 <mandira.sn.gowda@gmail.com>
2022-12-18 18:13:48 +0530

    Added Uc4 to add eventlistener for invalid password

--- js/simple_form.js
+++ js/simple_form.js
@@ -1,6 +1,7 @@
 const NAME_REGEX = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$');
 const EMAIL_REGEX = RegExp("[^[0-9A-Za-z]+(([._+-]?)[0-9A-Za-z]+)*@[0-9A-Za-z]+.[a-z]{2,4}.([a-z]{2,3})*$");
 const PHONE_REGEX = RegExp('^[0-9]{1,2}\\s{1}[0-9]{10}$');
+const PASSWORD_REGEX = RegExp('^[ A-Za-z0-9_@./$#&+-]{8,}$')
 
 const text = document.querySelector('#text');
 const textError = document.querySelector('.text-error');
@@ -36,6 +37,17 @@ telephone.addEventListener('input', function() {
     if (PHONE_REGEX.test(telephone.value)) {
         telephoneError.textContent = "";
     } else {
-        telephoneError.textContent = "MOBILE NUMBER Is Incorrect";
+        telephoneError.textContent = "MOBILE NUMBER Is Incorrect, add country code";
+    }
+});
+
+
+const password = document.querySelector('#pwd');
+const passwordError = document.querySelector('.pwd-error');
+password.addEventListener('input', function() {
+    if (PASSWORD_REGEX.test(password.value)) {
+        passwordError.textContent = "";
+    } else {
+        passwordError.textContent = "PASSWORD is short";
     }
 });
